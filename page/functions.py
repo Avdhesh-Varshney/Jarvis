5f911c7ffc12110ed16dae2803ff8ca575a89591
Avdhesh-Varshney <114330097+Avdhesh-Varshney@users.noreply.github.com>
2024-04-23 20:41:55 +0530

    main-functions
    Enlist and command the whole Jarvis program file.

---
+++ page/functions.py
@@ -0,0 +1,44 @@
+def load_functions():
+    functions = {"Admin Programs": [],
+                 "Super Admin Programs": [],
+                 "Basic Programs": [],
+                 "API Programs": [],
+    }
+
+    # Admin Functions
+    functions['Admin Programs'].append(['Code',
+                                        ]
+                                    )
+
+    # Super Admin Functions
+    functions['Super Admin Programs'].append(['Show Data',
+                                              ]
+                                            )
+
+    # Basic Functions
+    functions['Basic Programs'].append(['Advice Quotes', 
+                                        'My IP Address', 
+                                        'Jokes', 
+                                        'My Location'
+                                        ]
+                                    )
+
+    # API Functions
+    functions['API Programs'].append(['Calculator', 
+                                      'Temperature Condition', 
+                                      'Weather Condition', 
+                                      'Latest News', 
+                                      'Space News', 
+                                      'Mars Images', 
+                                      'Astroids', 
+                                      'Solar Bodies', 
+                                      'Movies', 
+                                      'TV Shows', 
+                                      'About Actors',
+                                      ]
+                                    )
+
+    # Return all the functions
+    return functions
+
+# print(load_functions())
---
+++ page/main.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import sys
+
+# core files
+from page.functions import load_functions
+
+# code files
+sys.path.insert(1, './source/')
+from source.Basic_Functions.English import Speak
+
+st.set_option("client.showSidebarNavigation", True)
+
+def main(data):
+    st.write(f'Welcome back, *{data[1]}*')
+    functions = load_functions()
+
+    service_list = [None] + list(functions.keys())
+    
+    if data[1] == 'Admin' and 'Super Admin Programs' in service_list:
+        service_list.remove('Super Admin Programs')
+    elif data[1] == 'User':
+        if 'Super Admin Programs' in service_list:
+            service_list.remove('Super Admin Programs')
+        if 'Admin Programs' in service_list:
+            service_list.remove('Admin Programs')
+
+    choice = st.selectbox('Services:', service_list)
+    if choice != None:
+        main_list = [None] + list(functions[choice])[0]
+        choice2 = st.selectbox('Programs:', main_list)
+        
+        if choice2 != None:
+            from source.Basic_Functions.Greeting import GreetUser
+            Speak(f"{GreetUser()}, It's Jarvis...")
+            Speak("Login Successfully!")
+    else:
+        st.info(f"Hello {data[0]}, Start your work!", icon="ℹ️")
