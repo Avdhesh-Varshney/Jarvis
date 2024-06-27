52d6ef7b242b38f884ed61100a46f11b0163324b
Sk <techjk021@gmail.com>
2024-06-27 13:51:10 +0530

    Added Jokes Program using API

--- src/apps/pages/programs/apiProgram.py
+++ src/apps/pages/programs/apiProgram.py
@@ -2,8 +2,10 @@ import streamlit as st
 
 def apiPrograms():
   st.title('API Programs')
-  choice = st.selectbox('Select a program to execute', [None])
-
+  choice = st.selectbox('Select a program to execute', [None, "Jokes"])
+  if choice == "Jokes":
+    from src.apps.pages.programs.ApiPrograms.Jokes import play_joke
+    play_joke()
   st.markdown('---')
 
 apiPrograms()
