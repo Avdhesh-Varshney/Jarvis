af28975cc044534ea67387aaa52cb20ac42fc266
krishnasharmak05 <krishnasharmak05@protonmail.com>
2024-07-01 16:07:07 +0530

    Removed secrets/.env

--- src/apps/pages/programs/ApiPrograms/genAIChatbot.py
+++ src/apps/pages/programs/ApiPrograms/genAIChatbot.py
@@ -1,8 +1,6 @@
 import google.generativeai as genai
 import os
 import streamlit as st
-from dotenv import load_dotenv
-
 
 # Initialization messages in session state to enable history.
 if "messages" not in st.session_state:
@@ -11,34 +9,23 @@ if "messages" not in st.session_state:
 
 def API_exists():
   # Check if the API exists. If it does, load it to the environment.
-  try:
-    load_dotenv(dotenv_path="src/apps/pages/programs/ApiPrograms/secrets/.env")
+  if os.environ.get("GOOGLE_API_KEY") != "":
     return "GOOGLE_API_KEY" in os.environ
-  except:
-    return False
+  return False
 
+
+def set_API():
+  st.markdown("Obtain an API key from [Google](%s) and enter it here:" % "https://ai.google.dev/gemini-api")
+  API = st.text_input(label="API Key")
+  if API != "":
+    os.environ["GOOGLE_API_KEY"] = API
 
 def instructions_to_set_API():
   # Helps an user get an API key from google.
-  if not API_exists():
-    st.markdown("ERROR!")
-    st.error(body="No API found!", icon=":material/warning:")
-    st.markdown("Here's how you can set the API yourself:")
-    st.markdown(
-        "Create an empty file called ```.env``` in the secrets folder found as ```src/apps/pages/programs/ApiPrograms/secrets```."
-    )
-    st.markdown(
-      "Then, obtain an API key for Gemini 1.5 Flash from [Google](%s) and add that to the ```.env``` file you just created in this format:"
-      % "https://ai.google.dev/gemini-api"
-    )
-    st.markdown("```GOOGLE_API_KEY=the-api-key-you-just-obtained```")
-    st.markdown(
-      "The chatbot will be usable until you delete that API key from the file or until you have reached the rate limits (if applicable) of the API key ."
-    )
-    st.markdown("Once you have added the API key to the file, press reload.")
-    if st.button("Reload"):
-			# Reload in order to show the chatbot page, instead of the page which helps the user get an API key.
-      st.rerun()
+  set_API()
+  if st.button("Enter"):
+  	# Reload in order to show the chatbot page, instead of the page which helps the user get an API key.
+    st.rerun()
 
 
 def init_chatbot():
@@ -62,10 +49,10 @@ def generate_response(chat_instance: genai.ChatSession, prompt: str):
       try:
         yield str(chunk.text)
       except ValueError as e:
-          st.error("ValueError: You are not allowed to ask that kind of a prompt!")      
+          st.error("ValueError: You are not allowed to ask that kind of a prompt!")
         
-		# Append response to session state for use in display_history function.
-    st.session_state.messages.append({"role": "ai", "content": response.text})
+	# Append response to session state for use in display_history function.
+  st.session_state.messages.append({"role": "ai", "content": response.text})
 
 
 def display_history():
