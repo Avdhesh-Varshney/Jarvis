55560cb379322dd281dccc272a0ff6b336622e96
krishnasharmak05 <krishnasharmak05@protonmail.com>
2024-07-01 16:06:24 +0530

    Changed name from "AI Chatbot" to "Gemini ChatBot"

--- src/apps/pages/programs/apiProgram.py
+++ src/apps/pages/programs/apiProgram.py
@@ -2,12 +2,12 @@ import streamlit as st
 
 def apiPrograms():
   st.title('API Programs')
-  choice = st.selectbox('Select a program to execute', [None, "Jokes", "AI Chatbot"])
+  choice = st.selectbox('Select a program to execute', [None, "Jokes", "Gemini ChatBot"])
   st.markdown('---')
   if choice == "Jokes":
     from src.apps.pages.programs.ApiPrograms.joke import play_joke
     play_joke()
-  elif choice == "AI Chatbot":
+  elif choice == "Gemini ChatBot":
     from src.apps.pages.programs.ApiPrograms.genAIChatbot import chatBot
     chatBot()
   else:
