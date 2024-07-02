98ef2b4c0bbae57ae4b1da36e82bf30f0679cc60
kom-senapati <komnoob123@gmail.com>
2024-07-02 21:58:47 +0530

    chore: changed Facts to General Facts

--- src/apps/pages/programs/apiProgram.py
+++ src/apps/pages/programs/apiProgram.py
@@ -2,12 +2,12 @@ import streamlit as st
 
 def apiPrograms():
   st.title('API Programs')
-  choice = st.selectbox('Select a program to execute', [None, "Jokes", "Facts"])
+  choice = st.selectbox('Select a program to execute', [None, "Jokes", "General Facts"])
   st.markdown('---')
   if choice == "Jokes":
     from src.apps.pages.programs.ApiPrograms.joke import play_joke
     play_joke()
-  elif choice == "Facts":
+  elif choice == "General Facts":
     from src.apps.pages.programs.ApiPrograms.fact import play_fact
     play_fact()
   else:
