fd05801d92bd779f03451b54ad4fc65faf2ba8ed
Vishal Maurya <40622097+vishalmaurya850@users.noreply.github.com>
2024-07-03 11:24:08 +0530

    Update imageProgram.py
    Created a QR code Generator markdown

--- src/apps/pages/programs/imageProgram.py
+++ src/apps/pages/programs/imageProgram.py
@@ -2,11 +2,16 @@ import streamlit as st
 
 def imagePrograms():
   st.title('Image Generator Programs')
-  choice = st.selectbox('Select a program to execute', [None, "Barcode Generator"])
+  choice = st.selectbox('Select a program to execute', [None, "Barcode Generator", "QR Code Generator"])
 
   st.markdown('---')
   if choice == "Barcode Generator":
     from src.apps.pages.programs.ImageGenerators.barcode_maker import gen_barcode
     gen_barcode()
 
+  st.markdown('---')
+  if choice == "QR Code Generator":
+    from src.apps.pages.programs.ImageGenerators.qr_code_generator import main
+    main()
+    
 imagePrograms()
