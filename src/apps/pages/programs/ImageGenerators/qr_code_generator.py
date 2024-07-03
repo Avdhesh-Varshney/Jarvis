376a1af513d2eab1362443a74b7c32bf2584974c
Vishal Maurya <40622097+vishalmaurya850@users.noreply.github.com>
2024-07-03 11:25:55 +0530

    Added QR Code Generator Program
    The QR Code Generator Program is added with Perfect UI and User Friendly

---
+++ src/apps/pages/programs/ImageGenerators/qr_code_generator.py
@@ -0,0 +1,61 @@
+import streamlit as st
+import qrcode
+from PIL import Image
+import io
+
+def main():
+    st.title("QR Code Generator")
+    
+    input_data = st.text_input("Enter data for the QR Code (text, link, number, etc.):")
+    
+    if st.button("Generate QR Code"):
+        if input_data:
+            qr = qrcode.QRCode(
+                version=1,
+                error_correction=qrcode.constants.ERROR_CORRECT_L,
+                box_size=10,
+                border=4,
+            )
+            qr.add_data(input_data)
+            qr.make(fit=True)
+            img = qr.make_image(fill_color="black", back_color="white")
+            
+            buf = io.BytesIO()
+            img.save(buf, format="PNG")
+            byte_im = buf.getvalue()
+            
+            st.image(byte_im, caption="Generated QR Code", use_column_width=True)
+
+            st.download_button(
+                label="Download QR Code",
+                data=byte_im,
+                file_name="qr_code.png",
+                mime="image/png"
+            )
+        else:
+            st.error("Please enter some data to generate the QR code.")
+
+    st.markdown(
+        f"""
+        <style>
+        .button-container {{
+            display: flex;
+            justify-content: space-around;  /* Adjust as needed: space-between, space-around, etc. */
+        }}
+        .styled-button {{
+            background-color: #ff4b4b;  /* Default button background color */
+            color: white;  /* Default button text color */
+            border: none;
+            padding: 10px 20px;
+            border-radius: 5px;
+            cursor: pointer;
+            transition: background-color 0.3s, color 0.3s;  /* Transition effect for color change */
+        }}
+        .styled-button:hover {{
+            background-color: #ff3333;  /* Button background color on hover */
+            color: white;  /* Button text color on hover */
+        }}
+        </style>
+        """,
+            unsafe_allow_html=True,
+        ) 
