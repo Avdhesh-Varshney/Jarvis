c018e794fa1527ecebeb78ec888dcd1b04560c09
kom-senapati <komnoob123@gmail.com>
2024-07-07 08:06:21 +0530

    code: world clock ✅

--- src/apps/pages/programs/apiProgram.py
+++ src/apps/pages/programs/apiProgram.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 def apiPrograms():
   st.title('API Programs')
-  choice = st.selectbox('Select a program to execute', [None, "Jokes", "General Facts", "Gemini ChatBot"])
+  choice = st.selectbox('Select a program to execute', [None, "Jokes", "General Facts", "Gemini ChatBot", "World Clock"])
   st.markdown('---')
   if choice == "Jokes":
     from src.apps.pages.programs.ApiPrograms.joke import play_joke
@@ -13,6 +13,9 @@ def apiPrograms():
   elif choice == "Gemini ChatBot":
     from src.apps.pages.programs.ApiPrograms.genAIChatbot import chatBot
     chatBot()
+  elif choice == "World Clock":
+    from src.apps.pages.programs.ApiPrograms.worldClock import display_world_clock
+    display_world_clock()
   else:
     st.info("Star this project on [GitHub](https://github.com/Avdhesh-Varshney/Jarvis), if you like it!", icon='⭐')
 
