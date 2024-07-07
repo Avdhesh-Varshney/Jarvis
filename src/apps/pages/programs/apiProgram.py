3c009a8cb3a1416c600c9f541fc6fe9cc37a6a5f
Akarsh Ghildyal <akarshghildyal@gmail.com>
2024-07-07 11:32:45 +0530

    feat : quote of the day

--- src/apps/pages/programs/apiProgram.py
+++ src/apps/pages/programs/apiProgram.py
@@ -2,11 +2,14 @@ import streamlit as st
 
 def apiPrograms():
   st.title('API Programs')
-  choice = st.selectbox('Select a program to execute', [None, "Jokes"])
+  choice = st.selectbox('Select a program to execute', [None, "Jokes", "Quote of the Day"])
   st.markdown('---')
   if choice == "Jokes":
     from src.apps.pages.programs.ApiPrograms.joke import play_joke
     play_joke()
+  elif choice == "Quote of the Day":
+    from src.apps.pages.programs.ApiPrograms.quotes import show_quote
+    show_quote()
   else:
     st.info("Star this project on [GitHub](https://github.com/Avdhesh-Varshney/Jarvis), if you like it!", icon='⭐')
 
