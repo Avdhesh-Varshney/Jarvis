97ede8bf0eef19e6774bf8bb30a05a491770926a
Akarsh Ghildyal <akarshghildyal@gmail.com>
2024-07-09 11:18:09 +0530

    feat : currency convertor

--- src/apps/pages/programs/apiProgram.py
+++ src/apps/pages/programs/apiProgram.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 def apiPrograms():
   st.title('API Programs')
-  choice = st.selectbox('Select a program to execute', [None, "Jokes", "General Facts", "Gemini ChatBot", "Quote of the Day"])
+  choice = st.selectbox('Select a program to execute', [None, "Jokes", "General Facts", "Gemini ChatBot", "Quote of the Day", "Currency Convertor"])
   st.markdown('---')
   if choice == "Jokes":
     from src.apps.pages.programs.ApiPrograms.joke import play_joke
@@ -15,7 +15,10 @@ def apiPrograms():
     chatBot()
   elif choice == "Quote of the Day":
     from src.apps.pages.programs.ApiPrograms.quotes import show_quote
-    show_quote()  
+    show_quote()
+  elif choice == "Currency Convertor":
+    from src.apps.pages.programs.ApiPrograms.currency import convert
+    convert()      
   else:
     st.info("Star this project on [GitHub](https://github.com/Avdhesh-Varshney/Jarvis), if you like it!", icon='⭐')
 
