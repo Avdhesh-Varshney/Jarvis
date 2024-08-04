b1aa9c24132ce8e34298009fbd6c4bdabbdd9d13
Avdhesh-Varshney <114330097+Avdhesh-Varshney@users.noreply.github.com>
2024-08-05 00:45:34 +0530

    debug-4

--- src/helpers/camelToReadable.py
+++ src/helpers/camelToReadable.py
@@ -1,3 +1,9 @@
+import streamlit as st
 def camelToReadable(camelCaseName):
+  st.markdown('---')
+  st.write(f"Original Camel Case Name: {camelCaseName}")
   readableName = ''.join([' ' + char if char.isupper() else char for char in camelCaseName]).strip()
+  st.write(f"Readable Name: {readableName}")
+  st.write(f"Readable Title Name: {readableName.title()}")
+  st.markdown('---')
   return readableName.title()
