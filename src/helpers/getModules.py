023d25b739a793f90fc601792e5009fb75ac4f42
Avdhesh-Varshney <114330097+Avdhesh-Varshney@users.noreply.github.com>
2024-08-05 00:28:26 +0530

    helper-func

---
+++ src/helpers/getModules.py
@@ -0,0 +1,17 @@
+import os
+import streamlit as st
+from src.helpers.camelToReadable import camelToReadable
+
+def getModules(COMMON_MODULE_PATH):
+  try:
+    files = [f for f in os.listdir(COMMON_MODULE_PATH) if f.endswith('.py') and not f.startswith('__')]
+  except FileNotFoundError:
+    st.error("The specified directory does not exist.")
+    return {}
+  
+  modules = {}
+  for file in files:
+    module_name = file[:-3]
+    readable_name = camelToReadable(module_name)
+    modules[readable_name] = module_name
+  return modules
