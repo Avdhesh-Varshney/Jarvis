bebdd73bef8fc9298954a849aeaf6342d52dced0
Avdhesh-Varshney <114330097+Avdhesh-Varshney@users.noreply.github.com>
2024-08-05 00:36:25 +0530

    debugging

--- src/apps/pages/programs/games.py
+++ src/apps/pages/programs/games.py
@@ -6,28 +6,30 @@ from src.helpers.getModules import getModules
 BASE_DIR = os.path.dirname(__file__)
 COMMON_MODULE_PATH = os.path.join(BASE_DIR, 'Games')
 
+# debugging
+st.write("Base Directory => ", BASE_DIR)
+st.write("Common Module Path => ", COMMON_MODULE_PATH)
+
 def games():
   st.title('🎮 Games 🕹️')
 
   modules = getModules(COMMON_MODULE_PATH)
   choice = st.selectbox('Select a program to execute', [None] + list(modules.keys()))
+  module_name = modules[choice]
   
   st.markdown('---')
 
   if choice in modules:
-    module_name = modules[choice]
-    
     try:
       module = importlib.import_module(f"src.apps.pages.programs.Games.{module_name}")
       func = getattr(module, module_name)
       func()
     except ModuleNotFoundError:
-      st.error(f"Module '{module_name}' could not be found.")
+      st.error(f"Module '{module_name}.py' could not be found.")
     except AttributeError:
-      st.error(f"Function '{module_name}' could not be found in '{module_name}'.")
+      st.error(f"Function '{module_name}' could not be found in '{module_name}.py'.")
     except Exception as e:
       st.error(f"An error occurred: {e}")
-
   else:
     st.info("Star this project on [GitHub](https://github.com/Avdhesh-Varshney/Jarvis), if you like it!", icon='⭐')
 
