159150ee7fe7df75c884296f90d043c4364f4c11
Avdhesh-Varshney <114330097+Avdhesh-Varshney@users.noreply.github.com>
2024-08-05 00:39:22 +0530

    debugging-2

--- src/apps/pages/programs/games.py
+++ src/apps/pages/programs/games.py
@@ -5,23 +5,27 @@ from src.helpers.getModules import getModules
 
 BASE_DIR = os.path.dirname(__file__)
 COMMON_MODULE_PATH = os.path.join(BASE_DIR, 'Games')
+MODULES = getModules(COMMON_MODULE_PATH)
 
 # debugging
 st.write("Base Directory => ", BASE_DIR)
 st.write("Common Module Path => ", COMMON_MODULE_PATH)
+st.write("Modules => ", MODULES)
 
 def games():
   st.title('🎮 Games 🕹️')
 
-  modules = getModules(COMMON_MODULE_PATH)
-  choice = st.selectbox('Select a program to execute', [None] + list(modules.keys()))
-  module_name = modules[choice]
+  choice = st.selectbox('Select a program to execute', [None] + list(MODULES.keys()))
+  module_name = MODULES[choice]
+  st.write("Choice => ", choice)
+  st.write("Module Name => ", module_name)
   
   st.markdown('---')
 
-  if choice in modules:
+  if choice in MODULES:
     try:
       module = importlib.import_module(f"src.apps.pages.programs.Games.{module_name}")
+      st.write("Module => ", module)
       func = getattr(module, module_name)
       func()
     except ModuleNotFoundError:
