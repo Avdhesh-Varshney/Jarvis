f753637654025d6d93a09afc8b297c6482505072
Avdhesh-Varshney <114330097+Avdhesh-Varshney@users.noreply.github.com>
2024-08-18 17:51:22 +0530

    whatsapp-addition

---
+++ src/apps/pages/automations/Messenger/whatsApp.py
@@ -0,0 +1,41 @@
+import pywhatkit
+import pandas as pd
+import pyautogui
+import time
+import streamlit as st
+
+def whatsApp():
+  st.title('WhatsApp Automation 📨')
+  st.write('This app sends a WhatsApp message to all the contacts in the CSV file.')
+
+  csv_file = st.file_uploader('Upload your CSV file', type=['csv'])
+  bottom = st.number_input('Enter the bottom range of phone numbers:', min_value=0, value=0)
+  top = st.number_input('Enter the top range of phone numbers:', min_value=bottom+1, value=bottom+1)
+
+  message = st.text_area('Enter the message to send:')
+
+  if st.button('Send WhatsApp Message') and csv_file is not None:
+    if message == '':
+      st.warning('Please enter a message to send.', icon="⚠️")
+      return
+    
+    df = pd.read_csv(csv_file)
+    if top > len(df):
+      st.warning('The top range of phone numbers exceeds the total number of phone numbers in the CSV file.', icon="⚠️")
+      return
+
+    if 'Phone Number' not in df.columns:
+      st.warning('Please upload a CSV file with a column named "Phone Number".', icon="⚠️")
+      return
+
+    for index, row in df.iterrows():
+      if index >= bottom and index < top:
+        pywhatkit.sendwhatmsg_instantly(row['Phone Number'], message, wait_time=10)
+        time.sleep(3)
+        pyautogui.press('enter')
+        time.sleep(1)
+        pyautogui.hotkey('ctrl', 'w')
+        st.write(f'Message sent to {row["Phone Number"]}')
+    st.write('All messages sent successfully!')
+  else:
+    st.info('Please upload a CSV file to send messages.', icon="ℹ️")
