6977b3896731b9d7a560c569fdeb1703f7f54569
Jai Dhingra <jaidhingra402@gmail.com>
2024-10-12 15:15:41 +0530

    Undo changes in imageProgram.py

--- src/apps/pages/programs/ImageGenerators/textToImageGenerator.py
+++ src/apps/pages/programs/ImageGenerators/textToImageGenerator.py
@@ -18,30 +18,24 @@ def load_pipeline():
     pipe.to(device)
     return pipe
 
-def camera_selection():
-    form_key = "Text_input_form"  # Use a unique key for the form
-    with st.form(form_key):
+# Define the main function
+def textToImageGenerator():
+    with st.form("Text_input_form"):
         st.session_state.input_text = st.text_input("Enter a prompt to generate Image:")
         
         start_button = st.form_submit_button("Generate Image")
         if start_button:
             st.session_state.running = True
-        
-        return st.session_state.input_text  # Return entered text
-
-# Define the main function
-def textToImageGenerator():
-    prompt = camera_selection()
     
-    if prompt:
+    if st.session_state.input_text:
         try:
             pipe = load_pipeline()
 
             if torch.cuda.is_available():
                 with torch.autocast("cuda"):
-                    output = pipe(prompt + " 4k, High Resolution", guidance_scale=8.5)
+                    output = pipe(st.session_state.input_text + " 4k, High Resolution", guidance_scale=8.5)
             else:
-                output = pipe(prompt + " 4k, High Resolution", guidance_scale=8.5)
+                output = pipe(st.session_state.input_text + " 4k, High Resolution", guidance_scale=8.5)
 
             image = output.images[0]
             st.image(image, caption="Generated Image", use_column_width=True)
@@ -54,5 +48,4 @@ def textToImageGenerator():
     else:
         st.warning("Please enter a prompt first.")
 
-# Call the main function directly
-textToImageGenerator()
+
