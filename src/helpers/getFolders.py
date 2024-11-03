5b4e641936ec25b5ea51ed26ad39aa5b8c5256d3
Avdhesh-Varshney <114330097+Avdhesh-Varshney@users.noreply.github.com>
2024-11-03 23:39:11 +0530

    indentation-fixing
    4 space separated tab-size setup

--- src/helpers/getFolders.py
+++ src/helpers/getFolders.py
@@ -3,14 +3,14 @@ import streamlit as st
 from src.helpers.camelToReadable import camelToReadable
 
 def getFolders(MODULE_FOLDER_PATH):
-  try:
-    files = [f for f in os.listdir(MODULE_FOLDER_PATH) if os.path.isdir(os.path.join(MODULE_FOLDER_PATH, f)) and not f.startswith('__')]
-  except FileNotFoundError:
-    st.error("The specified directory does not exist.")
-    return {}
+	try:
+		files = [f for f in os.listdir(MODULE_FOLDER_PATH) if os.path.isdir(os.path.join(MODULE_FOLDER_PATH, f)) and not f.startswith('__')]
+	except FileNotFoundError:
+		st.error("The specified directory does not exist.")
+		return {}
 
-  folders = {}
-  for folder in files:
-    readable_name = camelToReadable(folder)
-    folders[readable_name] = folder
-  return folders
+	folders = {}
+	for folder in files:
+		readable_name = camelToReadable(folder)
+		folders[readable_name] = folder
+	return folders
